6833d45884848262492b5ae37faa480a139cfe9e
Abraham Rivera <arivera@atl.yearup.org>
2015-04-24 16:14:22 -0400

    first program!!!

---
+++ Intro/src/Intro.java
@@ -0,0 +1,17 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// This is my first program
+		
+		System.out.println("hello world my name is Abraham Riera, welcome to java class");
+	}
+	
+}
+/* when i typed println with a capital letter the file did not run, which shows 
+ * how case sensitive the file is */
+
+// i also ran the file with {} instead of () and it did not want to run as well //
+
+/* i also attempted to run the program with a : instead of a ; at the end of the
+command line, but it does not run. The problem is pointed out. */
